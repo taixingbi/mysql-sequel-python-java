52e2d131e3e13d752246a5e93979fd1b6b910c1c
h <h@hs-MacBook-Pro.local>
2021-06-14 11:03:54 -0400

    update java

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ docker run --name mysql1 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=admin  -d mysql:5.7
 ### sequel
 ![Alt text](https://github.com/taixingbi/mysql-Sequel-Pro/blob/main/sequel.png)
 
-### aws sequel
+### aws sequel with aws instance
 ![Alt text](https://github.com/taixingbi/mysql-sequel-python-java/blob/main/images/awssequel.png)
             
 ### aws account
--- java-mysql/src/main/java/Demo.java
+++ java-mysql/src/main/java/Demo.java
@@ -1,11 +1,11 @@
 package com.dzone.albanoj2.maven.java;
 import java.sql.*;
+import java.util.*;
 
 public class Demo {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
+    static final String DB_URL = "jdbc:mysql://3.140.250.42:3306/TUTORIALSPOINT";
     static final String USER = "root";
     static final String PASS = "admin";
-    static final String QUERY = "SELECT id, first, last, age FROM Employees";
 
     public void demo() {
       System.out.print(DB_URL);
@@ -20,15 +20,32 @@ public class Demo {
       try{
         Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(QUERY);
-        // Extract data from result set
-        while (rs.next()) {
-          // Retrieve by column name
-          System.out.print("ID: " + rs.getInt("id"));
-          System.out.print(", Age: " + rs.getInt("age"));
-          System.out.print(", First: " + rs.getString("first"));
-          System.out.println(", Last: " + rs.getString("last"));
-        }
+
+        System.out.println("Inserting records into the table...");          
+        // String sql = "INSERT INTO Employees VALUES (100, 'Zara', 'Ali', 18)";
+        // stmt.executeUpdate(sql);
+        String sql = "INSERT INTO Employees VALUES (101, 'Mahnaz', 'Fatma', 25)";
+        stmt.executeUpdate(sql);
+        sql = "INSERT INTO Employees VALUES (102, 'Zaid', 'Khan', 30)";
+        stmt.executeUpdate(sql);
+        sql = "INSERT INTO Employees VALUES(103, 'Sumit', 'Mittal', 28)";
+        stmt.executeUpdate(sql);
+        System.out.println("Inserted records into the table...");  	  
+
+
+        // String SQL = "SELECT id, first, last, age FROM Employees";
+
+        // ResultSet rs = stmt.executeQuery(SQL);
+        // // Extract data from result set
+        // while (rs.next()) {
+        //   // Retrieve by column name
+        //   System.out.print("ID: " + rs.getInt("id"));
+        //   System.out.print(", Age: " + rs.getInt("age"));
+        //   System.out.print(", First: " + rs.getString("first"));
+        //   System.out.println(", Last: " + rs.getString("last"));
+        // }
+
+
 
       } catch (SQLException e) {
         e.printStackTrace();
@@ -36,3 +53,6 @@ public class Demo {
 
     }
 }
+
+
+
